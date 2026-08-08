63a106aad4ffb156709947548f1c19dda9bbc7dd
Narval <vaquitson@gmail.com>
2026-08-07 21:49:16 -0400

    adding the backbones of the cpu usage

--- libs/obc_hw_lib/fsw/src/obc_hw_lib.c
+++ libs/obc_hw_lib/fsw/src/obc_hw_lib.c
@@ -7,6 +7,7 @@
 float OBC_HW_LIB_get_cpu_temp(void);
 float OBC_HW_LIB_get_ram_usage_percentage(void);
 long int OBC_HW_LIB_get_ram_usage(void);
+float OBC_HW_LIB_get_cpu_usage(void);
 
 #ifdef OBC_PLATFORM_LINUX
 #include "narwal_thermal_zones.h"
@@ -27,12 +28,43 @@ float OBC_HW_LIB_get_cpu_temp(void){
   return narwal_thermal_zones_get_temp(tz_p);
 }
 
+
 float OBC_HW_LIB_get_ram_usage_percentage(void){
   return narwal_ram_usage_percentage();
 }
 
 long int OBC_HW_LIB_get_ram_usage(void){ 
   return narwal_ram_usage();
+}
+
+float OBC_HW_LIB_get_cpu_usage(){
+
+
+  idle_time_1 = narwal_cpu_idle_time(&cpu);
+  busy_time_1 = narwal_cpu_busy_time(&cpu);
+
+  close(stat_fd);
+
+  stat_fd = open("tests/test_files/stat_2", O_RDONLY);
+  if (stat_fd < 0){
+    return -2;
+  }
+
+  *priv_narwal_cpu_get_fd() = stat_fd;
+     
+  // 100 ms
+  usleep(100000); 
+
+  idle_time_2 = narwal_cpu_idle_time(&cpu);
+  busy_time_2 = narwal_cpu_busy_time(&cpu);
+
+  NarwalCpuTime_t busy_delta = busy_time_2 - busy_time_1;
+  NarwalCpuTime_t idle_delta = idle_time_2 - idle_time_1;
+
+  percentage = 100.0 * busy_delta / (busy_delta + idle_delta);  
+
+
+  return
 }
 
 #else
